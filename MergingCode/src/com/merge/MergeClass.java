60065696998bff337d46dd7beb8b2bb771a7a1f0
ravilella1234 <ravilella1234>
2019-07-31 06:50:26 +0530

    Added code for Addition

--- MergingCode/src/com/merge/MergeClass.java
+++ MergingCode/src/com/merge/MergeClass.java
@@ -2,8 +2,14 @@ package com.merge;
 
 public class MergeClass {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		int a=10, b=20, c=0;
+		
+		//Addition
+		c=a+b;
+		
+		System.out.println("Addition of A & B is  :" + c);
 
 	}
 
